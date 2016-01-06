cb1585e64c36b704c0a4a0aa0935ebf3a79cd7b2
Tim Wieschalla <Tim.Wieschalla@gmail.com>
2016-01-06 17:59:32 +0100

    test wait script

--- c/cjOld/waitScript.sh
+++ c/cjOld/waitScript.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-until [ "`curl --silent --show-error --connect-timeout 1 -I http://localhost:8080 | grep 'Coyote'`" != "" ];
+# until [ "`curl --silent --show-error --connect-timeout 1 -I http://localhost:8080 | grep 'Coyote'`" != "" ];
+until [ "`curl --silent --show-error --connect-timeout 1 -I http://localhost:8080 | grep '302 Found'`" != "" ];
+# 302 Found
+# source: http://stackoverflow.com/questions/376279/wait-until-tomcat-finishes-starting-up
 do
   echo --- sleeping for 10 seconds
   sleep 10
 done
 
-echo Tomcat is ready!
+echo Jenkins is ready!
