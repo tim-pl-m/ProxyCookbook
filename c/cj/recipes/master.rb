ce30c019e197fe71b714e6a31a56d15b74eacec6
Tim Wieschalla <tim.wieschalla@adesso.de>
2016-01-12 13:00:55 +0100

    activate plugins

--- c/cj/recipes/master.rb
+++ c/cj/recipes/master.rb
@@ -9,18 +9,18 @@
 
 include_recipe "java::default"
 include_recipe "java::set_java_home"
-# include_recipe "jenkins::master"
-# if node["ciexjenkins"]["install_plugins"]
-#   include_recipe "ciexjenkins::plugins"
-# end
-#
-# cron 'cleanup' do
-#   minute '0'
-#   command 'find /tmp/ -maxdepth 1 -user jenkins'\
-#           ' -mmin +720 \( ! -name "hsperfdata_*" \)'\
-#           ' -exec rm -rf {} \; >/dev/null 2>&1'
-# end
-#
-# if node["ciexjenkins"]["ad"]["enabled"]
-#   include_recipe "ciexjenkins::ad"
-# end
+include_recipe "jenkins::master"
+if node["ciexjenkins"]["install_plugins"]
+  include_recipe "ciexjenkins::plugins"
+end
+
+cron 'cleanup' do
+  minute '0'
+  command 'find /tmp/ -maxdepth 1 -user jenkins'\
+          ' -mmin +720 \( ! -name "hsperfdata_*" \)'\
+          ' -exec rm -rf {} \; >/dev/null 2>&1'
+end
+
+if node["ciexjenkins"]["ad"]["enabled"]
+  include_recipe "ciexjenkins::ad"
+end
