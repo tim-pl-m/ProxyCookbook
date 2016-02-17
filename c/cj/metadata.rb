75a7f4929c42711211393cce1af3ef3e01af7637
Tim Wieschalla <tim.wieschalla@adesso.de>
2016-02-17 19:17:59 +0100

    act plugins and verify

--- c/cj/metadata.rb
+++ c/cj/metadata.rb
@@ -6,8 +6,11 @@ description 'Configures a Jenkins service with Docker slave support'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 
-depends 'java', '= 1.31.0'
-depends 'jenkins', '= 2.4.1'
+# depends 'java', '= 1.31.0'
+# depends 'jenkins', '= 2.4.1'
+
+depends 'java'
+depends 'jenkins'
 
 recipe 'master', 'Installs and configures a Jenkins master node'
 recipe 'ad', 'Installs Active Directory plugin and configures it'
