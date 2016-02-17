0bcb8409dc2efd1e0b3757fb3e80d5e9c0a1f732
Tim Wieschalla <Tim.Wieschalla@gmail.com>
2016-02-17 15:15:18 +0100

    kitchen docker

--- c/cj/metadata.rb
+++ c/cj/metadata.rb
@@ -7,8 +7,10 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 
 # depends 'java', '= 1.31.0'
+# depends 'jenkins', '= 2.4.1'
+
 depends 'java'
-depends 'jenkins', '= 2.4.1'
+depends 'jenkins'
 
 recipe 'master', 'Installs and configures a Jenkins master node'
 recipe 'ad', 'Installs Active Directory plugin and configures it'
