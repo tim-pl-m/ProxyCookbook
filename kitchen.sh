beea4fbf10cc3e04fcf0cefdcdc9cfafab2236cf
Tim Wieschalla <Tim.Wieschalla@gmail.com>
2016-06-23 14:43:28 +0200

    check berkshelf

--- kitchen.sh
+++ kitchen.sh
@@ -16,6 +16,10 @@ ruby -v
 kitchen version
 # bundle install
 # kitchen list
+
+# check if berkshelf installed
+gem install berkshelf
+
 KITCHEN_LOCAL_YAML=.kitchen.docker.yml kitchen list
 # KITCHEN_LOCAL_YAML=.kitchen.docker.yml kitchen converge
 #kitchen verify
