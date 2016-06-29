e99beb7f0eb5772d89c9b0afb84c673a6da7ee49
Tim Wieschalla <Tim.Wieschalla@gmail.com>
2016-06-29 15:11:49 +0200

    add verify

---
+++ kitchen_verify.sh
@@ -0,0 +1,47 @@
+
+# ----
+# jenkins commands:
+# ls
+# pwd
+#
+# git --version
+#
+# git clone https://github.com/tim-pl-m/ProxyCookbook.git --branch ci-role_fromContainer
+# git log -1 --pretty=%B
+# cd ProxyCookbook
+# bash kitchen.sh
+# ----
+
+ls
+git status
+
+
+
+
+
+pwd
+# show hidden files
+ls -a
+
+ruby -v
+kitchen version
+
+# kitchen list
+
+whoami
+
+# check if berkshelf installed
+# sudo gem install berkshelf
+
+#go to jenkins-cookbook
+cd c/cj
+
+bundle install
+
+KITCHEN_LOCAL_YAML=.kitchen.docker.yml kitchen list
+KITCHEN_LOCAL_YAML=.kitchen.docker.yml kitchen converge
+KITCHEN_LOCAL_YAML=.kitchen.docker.yml kitchen verify
+
+#kitchen verify
+
+# noop1
