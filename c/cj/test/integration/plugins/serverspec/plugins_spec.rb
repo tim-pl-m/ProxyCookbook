c1c1a2034a71aa599cd9b9834456a5ade6e448d4
Tim Wieschalla <Tim.Wieschalla@gmail.com>
2016-06-29 16:29:32 +0200

    deactivate plugin-test manually

--- c/cj/test/integration/plugins/serverspec/plugins_spec.rb
+++ c/cj/test/integration/plugins/serverspec/plugins_spec.rb
@@ -7,10 +7,14 @@ require_relative "support/spec_helper"
 require_relative "examples/plugins"
 require_relative "examples/jenkins_service"
 
-describe "installed plugins" do
-  include_examples "jenkins plugins"
-end
-
 describe "jenkins service" do
   it_behaves_like "jenkins service"
 end
+
+# TODO deactivate somewhere else!!!1!
+
+# describe "installed plugins" do
+#   include_examples "jenkins plugins"
+# end
+
+
