e6baa702fa37d9d4c766f2691e37f548d25bbaf9
Tim Wieschalla <Tim.Wieschalla@gmail.com>
2016-08-10 22:49:42 +0200

    test

--- provision.sh
+++ provision.sh
@@ -1,4 +1,5 @@
-if [ $color = auto ]; then
-  if [ -t 1 ]; then color=yes; else color=no; fi
-fi
+# if [ $color = auto ]; then
+#   if [ -t 1 ]; then color=yes; else color=no; fi
+# fi
+color=yes
 bundle install
