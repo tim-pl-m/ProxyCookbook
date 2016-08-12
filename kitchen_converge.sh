87a1ac1205df0787e68416ae9e20d27e49a05748
Tim Wieschalla <Tim.Wieschalla@gmail.com>
2016-08-12 13:36:23 +0200

    add provision and jenkins shell

--- kitchen_converge.sh
+++ kitchen_converge.sh
@@ -1,16 +1,4 @@
 
-# ----
-# jenkins commands:
-# ls
-# pwd
-#
-# git --version
-#
-# git clone https://github.com/tim-pl-m/ProxyCookbook.git --branch ci-role_fromContainer
-# git log -1 --pretty=%B
-# cd ProxyCookbook
-# bash kitchen.sh
-# ----
 
 ls
 git status
@@ -45,3 +33,52 @@ KITCHEN_LOCAL_YAML=.kitchen.docker.yml kitchen converge
 #kitchen verify
 
 # noop1
+
+# ---
+# # jenkins job "kitchen converge jenkins"
+# set -x
+# # to hide "echo"
+#
+# ls
+# pwd
+#
+# git --version
+#
+# git clone https://github.com/tim-pl-m/ProxyCookbook.git --branch ci-role_fromContainer --depth 1
+# ls
+# cd ProxyCookbook
+# ls
+# git log -1
+# #git log -1 --pretty=%B
+# #gem install berkshelf
+#
+#
+# #vboxmanage --version
+#
+# #source ~/.bash_profile
+# #source /etc/profile.d/rvm.sh
+#
+# ruby -v
+# #gem install test-kitchen
+# kitchen -v
+# #gem install bundler
+# bundler -v
+# #TODO check if vagrant needed for docker
+# #sudo gem install vagrant
+# #vagrant -v
+# echo -e "\033[43m start provisioning \033[0m"
+#
+# git clone https://github.com/tim-pl-m/bundler
+# cd bundler
+# rake build
+# sudo gem install pkg/bundler-1.13.0.rc.1.gem
+# bundle -v
+# cd ..
+# bash provision.sh
+#
+# bash kitchen_converge.sh
+# echo -e "\033[42m Success \033[0m"
+# # color needs: https://wiki.jenkins-ci.org/display/JENKINS/AnsiColor+Plugin
+#
+#
+#
