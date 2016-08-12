4e509048383ac48dceab0ccb6502fc9698745e99
Tim Wieschalla <Tim.Wieschalla@gmail.com>
2016-08-12 11:25:00 +0200

    remove sudo before gem

--- provision.sh
+++ provision.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/tim-pl-m/bundler
 cd bundler
 rake build
-sudo gem install pkg/bundler-1.13.0.rc.1.gem
+gem install pkg/bundler-1.13.0.rc.1.gem
 bundle -v
 bundle install
