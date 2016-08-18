4b953f62990bd33d60001644d7193025abf9f398
Tim Wieschalla <Tim.Wieschalla@gmail.com>
2016-08-18 16:24:52 +0200

    bundler build in vsys

--- provision.sh
+++ provision.sh
@@ -1,8 +1,10 @@
-# git clone https://github.com/tim-pl-m/bundler
-# cd bundler
-# rake build
-# gem install pkg/bundler-1.13.0.rc.1.gem
-# bundle -v
+git clone https://github.com/tim-pl-m/bundler
+cd bundler
+rake build
+sudo gem install pkg/bundler-1.13.0.rc.1.gem
+bundle -v
+cd ..
+
 gem list
 bundle install
 gem list
