3c54526dff53e197def44a5ab1493ec3f144be75
Tim Wieschalla <Tim.Wieschalla@gmail.com>
2016-08-19 18:45:35 +0200

    add docker test

--- test/integration/default/serverspec/localhost/default_spec.rb
+++ test/integration/default/serverspec/localhost/default_spec.rb
@@ -15,3 +15,15 @@ require 'spec_helper'
      end
    end
  end
+
+ describe 'docker installed' do
+  #  describe command 'sudo service httpd status' do
+  #    its(:stdout) { is_expected.to match(/httpd \(pid\s\s.*is\srunning/) }
+  #    # httpd (pid  3047) is running...
+  #  end
+   describe command 'docker -v' do
+    #  its(:stderr) { is_expected.to match(/git version 1\.7\..*/) }
+     its(:stdout) { is_expected.to match(/Docker version*/) }
+    #  Docker version 1.7.1, build 786b29d/1.7.1
+   end
+ end
