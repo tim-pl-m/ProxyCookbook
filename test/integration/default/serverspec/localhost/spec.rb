3cec21f237edca48b94910d6f572ef0899f69e43
Tim Wieschalla <Tim.Wieschalla@gmail.com>
2016-08-19 18:30:08 +0200

    add tests

---
+++ test/integration/default/serverspec/localhost/spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+ if os[:family] == 'ubuntu'
+       describe '/etc/lsb-release' do
+         it "exists" do
+             expect(file('/etc/lsb-release')).to be_file
+         end
+       end
+ end
+
+ if os[:family] == 'redhat'
+   describe '/etc/redhat-release' do
+     it "exists" do
+         expect(file('/etc/redhat-release')).to be_file
+     end
+   end
+ end
