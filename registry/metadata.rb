f5d3f1ed378fedc6834e849d8bf5082fcf4b849b
Tim Wieschalla <Tim.Wieschalla@gmail.com>
2016-08-23 21:29:11 +0200

    nginx /w docker-compose

--- registry/metadata.rb
+++ registry/metadata.rb
@@ -1,21 +1,15 @@
 name 'registry'
-maintainer 'Max Edenharter'
-maintainer_email 'max.edenharter@adesso.de'
+maintainer 'Tim Wieschalla'
+maintainer_email 'tim.wieschalla@adesso.de'
 license 'All rights reserved'
-description 'Configures a Jenkins service with Docker slave support'
+description 'wip'
 # TODO check for readme?
 # long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 
-# depends 'java', '= 1.31.0'
-# depends 'jenkins', '= 2.4.1'
-# depends 'docker'
-# depends 'java'
-# depends 'jenkins'
 depends 'docker'
-#
-# recipe 'master', 'Installs and configures a Jenkins master node'
-# recipe 'ad', 'Installs Active Directory plugin and configures it'
-# recipe 'plugins', 'Installs all defined plugins.'
+depends 'docker_compose'
 
 supports 'centos', '~> 7'
+
+#berks install
--- registry/recipes/default.rb
+++ registry/recipes/default.rb
@@ -7,11 +7,25 @@
 # All rights reserved - Do Not Redistribute
 #
 
-# This declaration is only necessary if a non-master recipe is run stand-alone
-# service "jenkins" do
-#   action :nothing
-# end
-
 docker_service 'default' do
   action [:create, :start]
 end
+
+include_recipe 'docker_compose::installation'
+
+# Provision Compose file
+cookbook_file '/etc/docker-compose_nginx.yml' do
+  source 'docker-compose_nginx.yml'
+  owner 'root'
+  group 'root'
+  mode 0640
+  notifies :up, 'docker_compose_application[nginx]', :delayed
+end
+
+# Provision Compose application
+docker_compose_application 'nginx' do
+  action :up
+  compose_files [ '/etc/docker-compose_nginx.yml' ]
+end
+
+
